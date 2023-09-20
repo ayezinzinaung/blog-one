204d526e17fc8076a5fe18bead2d54b2f586e917
Aye Zin Zin Aung <ayezin2aung101120@gmail.com>
2023-09-20 15:12:26 +0630

    .

---
+++ database/migrations/2023_09_11_094213_create_posters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql2')->create('posters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('mysql2')->dropIfExists('posters');
+    }
+}
